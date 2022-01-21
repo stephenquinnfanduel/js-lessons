63222fc2c6c197898b2daf9441e63643df6d4a23
stephenquinnfanduel <stephen.quinn@fanduel.com>
2022-01-21 11:35:10 +0000

    Add first lesson tasks

---
+++ tasks/lesson1-tasks.js
@@ -0,0 +1,31 @@
+/*
+## Tasks
+
+### Task 1 - Variables
+**Part 1**: We want to store some data about a person. Use a real or fictional example to create variables called `firstName`, `secondName`, `age`, and `job`. 
+
+**Part 2**: A year's passed since you first wrote that code. Time flies haha! [note to self - don't add back jokes]! Make your person a year older.
+
+### Task 2: Types
+**Part 1**: Below is a list of containing 6 values. Write down their types next to them.
+
+```JS
+"This is alright."
+
+42
+
+true
+
+undefined
+
+null
+
+{}
+```
+
+### Task 3: Functions
+**Part 1**: Write a greet function which says "Good morning!". You can call the function anything but try to give it a useful name.
+
+**Part 2**: Copy the function from Part 1 and change it to greet someone by name. We should be able to use any name with it. I.e. use it to say "Good morning Alastair!" or "Good morning Crawford!"
+
+**Part 3**: Write a function to add two numbers. Just as how we could use any name for the function in Part 2, we should be able to use any numbers we want for this part. */
