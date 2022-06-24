cfd8d3d98ec55f7db89d9fd3f4a0c504ace48a7d
stephenquinnfanduel <stephen.quinn@fanduel.com>
2022-06-24 12:16:48 +0100

    initial solution using boolean value

--- light-switch/app.js
+++ light-switch/app.js
@@ -1,1 +1,27 @@
 console.log("loaded")
+const initialButtonState = {
+    buttonLabel : "Light is On",
+    roomColour : "yellow",
+    isLightOn : true
+}
+
+const clickButton = () => {
+    if (initialButtonState.isLightOn === true){
+        initialButtonState.isLightOn = false
+        btn.innerHTML = "Light is Off"
+        document.body.style.background = "white"
+    }
+    else{
+        initialButtonState.isLightOn = true
+        btn.innerHTML = initialButtonState.buttonLabel
+        document.body.style.background = initialButtonState.roomColour
+    }
+}
+
+const btn = document.querySelector("#light-switch")
+
+btn.innerHTML = initialButtonState.buttonLabel
+document.body.style.background = initialButtonState.roomColour
+
+btn.addEventListener("click", clickButton)
+
