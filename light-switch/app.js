61fefd1d1e2a67d1537f7b1efebb868b2228b530
stephenquinnfanduel <stephen.quinn@fanduel.com>
2022-06-24 11:26:27 +0100

    initial solution with button click count

--- light-switch/app.js
+++ light-switch/app.js
@@ -1,1 +1,21 @@
 console.log("loaded")
+
+const isOdd = numberToCheck => numberToCheck % 2 === 1 
+let buttonClicks = 0
+
+const btn = document.createElement("button")
+btn.innerHTML = "Switch On"
+document.body.style.background = 'yellow';
+btn.onclick = () => {
+    buttonClicks = buttonClicks +1
+    if (isOdd(buttonClicks) === true){
+        btn.innerHTML = "Switch Off"
+        document.body.style.background = 'white';
+    }
+    else {
+        btn.innerHTML = "Switch On"
+        document.body.style.background = 'yellow';
+    }
+}
+  
+document.body.appendChild(btn)
